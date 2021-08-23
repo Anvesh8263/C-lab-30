98830c34d7342fb12697b6aabb2ec646c8bb2af4
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-08-23 12:16:16 +0530

    Add files via upload

---
+++ Area_Of_Square.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+    int main() {
+                  int side, area;
+                  printf("Give the Length of Side : ");
+                  scanf("%d", &side);
+                  area = side * side;
+                  printf("\n\n The Area of Square is : %d", area);
+    return (0);
+    }
