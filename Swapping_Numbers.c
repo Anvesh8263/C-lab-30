bd3b656065b28fb43f27eff32a9babb75adb5b00
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-08-23 12:17:42 +0530

    Add files via upload

---
+++ Swapping_Numbers.c
@@ -0,0 +1,8 @@
+#include<stdio.h> 
+int main() { 
+double first, second, temp; 
+printf("Enter first number: "); 
+scanf("%lf", &first); 
+printf("Enter second number: ");
+ scanf("%lf", &second); 
+ printf("\nAfter swapping, firstNumber = %.2lf\n", first); printf("After swapping, secondNumber = %.2lf", second); return 0; }
